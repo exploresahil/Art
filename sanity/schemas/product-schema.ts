027bd71b039603d7556367417cf7d01538eebded
paragkoche <paragkoche0@gmail.com>
2024-04-27 21:16:46 +0530

    update

--- sanity/schemas/product-schema.ts
+++ sanity/schemas/product-schema.ts
@@ -1,4 +1,4 @@
-import { defineField, defineType } from "sanity";
+import { defineArrayMember, defineField } from "sanity";
 import { unsplashAssetSource } from "sanity-plugin-asset-source-unsplash";
 
 const productsSchema = {
@@ -6,13 +6,13 @@ const productsSchema = {
   title: "Products",
   name: "products",
   fields: [
-    {
+    defineField({
       name: "name",
       title: "Name",
       type: "string",
       description: "Enter Product Name",
-    },
-    {
+    }),
+    defineField({
       name: "slug",
       title: "Slug",
       type: "slug",
@@ -21,46 +21,51 @@ const productsSchema = {
         maxLength: 90,
       },
       description: "Click Generate to generate slug",
-    },
-    {
+    }),
+    defineField({
       name: "category",
       title: "Category",
       type: "reference",
       to: [{ type: "productCategory" }],
       description: "Select Category of the product",
-    },
-    {
+    }),
+    defineField({
       name: "images",
       title: "Images",
       type: "array",
-      description: "Add one ore more product Images",
-
+      description: "Add one or more product Images",
       of: [
         {
           type: "image",
-          options: { hotspot: true },
-          sources: [unsplashAssetSource],
+          options: {
+            hotspot: true,
+            sources: [unsplashAssetSource],
+          },
         },
       ],
       options: {
         layout: "grid",
       },
-      validation: (Rule: any) => Rule.required(),
-    },
-    {
+      validation: (val) => val.required(),
+    }),
+    defineField({
       name: "description",
       title: "Description",
       type: "array",
-      of: [{ type: "block" }],
+      of: [
+        defineArrayMember({
+          type: "block",
+        }),
+      ],
       description: "Enter description of the product",
-    },
-    {
+    }),
+    defineField({
       name: "isAvailable",
       title: "Is Available",
       type: "boolean",
       description: "Select true if this product is Available, false otherwise",
       initialValue: false,
-    },
+    }),
   ],
 };
 
