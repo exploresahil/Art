0fd78610a5a4df023688aaa76d449c59783ff6a1
Your Name <you@example.com>
2024-05-18 18:20:15 +0530

    update local store data redux

--- src/Providers/ReduxProvider.tsx
+++ src/Providers/ReduxProvider.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from "react";
 import { Provider } from "react-redux";
 import { makeStore, AppStore } from "@/src/lib/store";
 import { setupListeners } from "@reduxjs/toolkit/query";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function StoreProvider({
   children,
@@ -20,10 +21,16 @@ export default function StoreProvider({
     if (storeRef.current != null) {
       // configure listeners using the provided defaults
       // optional, but required for `refetchOnFocus`/`refetchOnReconnect` behaviors
-      const unsubscribe = setupListeners(storeRef.current.dispatch);
+      const unsubscribe = setupListeners(storeRef.current.store.dispatch);
       return unsubscribe;
     }
   }, []);
 
-  return <Provider store={storeRef.current}>{children}</Provider>;
+  return (
+    <Provider store={storeRef.current.store}>
+      <PersistGate persistor={storeRef.current.persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
--- src/lib/store.ts
+++ src/lib/store.ts
@@ -2,21 +2,31 @@ import type { Action, ThunkAction } from "@reduxjs/toolkit";
 import { combineSlices, configureStore } from "@reduxjs/toolkit";
 import { BrandSlice } from "./reducer/brandSlice.reducer";
 import { CartSlice } from "./reducer/CardSlice.reducer";
-
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+const persistConfig = {
+  key: "root",
+  storage,
+};
 const rootReducer = combineSlices(BrandSlice, CartSlice);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 export type RootState = ReturnType<typeof rootReducer>;
 
 export const makeStore = () => {
-  return configureStore({
-    reducer: rootReducer,
+  const store = configureStore({
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) => {
       return getDefaultMiddleware().concat();
     },
   });
+  return {
+    store,
+    persistor: persistStore(store),
+  };
 };
 
 export type AppStore = ReturnType<typeof makeStore>;
-export type AppDispatch = AppStore["dispatch"];
+export type AppDispatch = AppStore["store"]["dispatch"];
 export type AppThunk<ThunkReturnType = void> = ThunkAction<
   ThunkReturnType,
   RootState,
