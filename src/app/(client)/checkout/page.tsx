19518eee711c753db3865066da23aa8b7c799593
Your Name <you@example.com>
2024-05-19 16:31:39 +0530

    update checkout page

--- src/app/(client)/checkout/page.tsx
+++ src/app/(client)/checkout/page.tsx
@@ -1,4 +1,14 @@
+"use client";
+import { useAppSelector } from "@/src/lib/hook";
+import { GetCart } from "@/src/lib/reducer/CardSlice.reducer";
+import { useRouter } from "next/navigation";
+
 const page = () => {
+  const data = useAppSelector(GetCart);
+  const router = useRouter();
+  if (data.length == 0) {
+    router.push("/404");
+  }
   return <div>page</div>;
 };
 
